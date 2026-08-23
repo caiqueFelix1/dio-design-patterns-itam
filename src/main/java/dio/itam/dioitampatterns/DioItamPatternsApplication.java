ee7a82bd5c09275a6a5cce490bd73747dc83aee8
Caíque Felix <caique.fm@gmail.com>
2026-08-22 23:20:29 -0300

    feat: implement State Design Pattern for ITAM asset lifecycle management

---
+++ src/main/java/dio/itam/dioitampatterns/DioItamPatternsApplication.java
@@ -0,0 +1,13 @@
+package dio.itam.dioitampatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioItamPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioItamPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dio/itam/dioitampatterns/DioItamPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.itam.dioitampatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioItamPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
